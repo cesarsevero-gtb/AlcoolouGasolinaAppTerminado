084f955966bbffbc5b9e15842b5f422525f4a28a
César severo <direito1boab@gmail.com>
2022-08-18 13:57:48 -0300

    Adicionado SplashScreen

---
+++ app/src/main/java/com/getword/alcoolougasolina/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.getword.alcoolougasolina
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        Handler().postDelayed( {
+
+            //code
+            startActivity(Intent(this, MainActivity::class.java))
+
+        }, 3000)
+
+    }
+}
